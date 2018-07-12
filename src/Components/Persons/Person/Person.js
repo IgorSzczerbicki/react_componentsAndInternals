da81f19467be8d21eccb91df42031ee1dcebba68
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-12 08:48:48 +0200

    lifeCycle komponentu

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -2,7 +2,22 @@ import React, {Component} from 'react'
 import classes from './Person.css'
 
 class Person extends Component {
+
+	constructor(props) {
+		super(props);
+		console.log('[Person.js] Constructor', props)
+	}
+
+	componentWillMount(){
+		console.log('[Person.js] componentWillMount()')
+	}
+
+	componentDidMount(){
+		console.log('[Person.js] componentDidMount()')
+	}
+
 	render() {
+		console.log('[Person.js] render()');
 		return (
 			<div className={classes.Person}>
 				<p onClick={this.props.delete}>
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,19 @@ import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
 
+	constructor(props) {
+		super(props);
+		console.log('[App.js] Constructor', props)
+	}
+
+	componentWillMount(){
+		console.log('[App.js] componentWillMount()')
+	}
+
+	componentDidMount(){
+		console.log('[App.js] componentDidMount()')
+	}
+
 	state = {
 		persons: [
 			{id: 0, name: 'Igor', age: 30},
@@ -44,6 +57,7 @@ class App extends Component {
 	};
 
 	render() {
+		console.log('[App.js] render()');
 		let persons = null;
 
 		if (this.state.showPersons){

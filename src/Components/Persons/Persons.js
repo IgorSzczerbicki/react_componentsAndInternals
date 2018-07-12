94f3a2349f16a660a15d086ab644e1a1581ed692
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-12 08:49:21 +0200

    lifeCycle aktualizowanie przez rodzica

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -16,6 +16,23 @@ class Persons extends Component{
 		console.log('[Persons.js] componentDidMount()')
 	}
 
+	componentWillReceiveProps(nextProps){
+		console.log('[UPDATE Persons.js] componentWillReceiveProps()', nextProps)
+	}
+
+	shouldComponentUpdate(nextProps, nextState){
+		console.log('[UPDATE Persons.js] shouldComponentUpdate()', nextProps, nextState);
+		return nextProps.persons !== this.props.persons;
+	}
+
+	componentWillUpdate(nextProps, nextState){
+		console.log('[UPDATE Persons.js] componentWillUpdate()', nextProps, nextState)
+	}
+
+	componentDidUpdate(){
+		console.log('[UPDATE Persons.js] componentDidUpdate()')
+	}
+
 	render() {
 		console.log('[Persons.js] render()');
 		return this.props.persons.map((person, index) => {
